d4e0971437246ba23c96b2a3992e786a820996a9
hmjwcp <1181871421@qq.com>
2020-03-30 23:52:54 +0800

    first20200330

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import './App.css';
-import { Button } from 'antd';
-
-
-function App() {
-  return (
-    <div className="App">
-
-      <Button type="primary">Button</Button>
-    </div>
-  );
+import React, {Component} from 'react'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
+/*
+应用根组件
+*/
+class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route path='/login' component={Login}/>
+                    <Route path='/' component={Admin}/>
+                </Switch>
+            </BrowserRouter>
+        )
+    }
 }
 
 export default App;
